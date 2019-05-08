90538d139ab0fa27667cc5cecd36c32d8ff0075f
idoye <idoye@campus.technion.ac.il>
2019-05-08 13:03:01 +0300

    implement supervisor

--- HW_2/matrix_server.erl
+++ HW_2/matrix_server.erl
@@ -2,7 +2,7 @@
 -author("Ido Yehezkel & Ohad Zohar").
 
 %% API
--export([mult/2, shutdown/0]).
+-export([mult/2, get_version/0, shutdown/0, start_server/0, explanation/0, matrix_server/0]).
 
 % generate a matrix with X rows and Y columns with zeros
 getZeroMat(X, Y) ->
@@ -61,27 +61,60 @@ matricesMultiply(Matrix1, Matrix2, ClientPid, MsgRef) ->
   ClientPid ! {MsgRef, ResMatrix}.
 
 
-
-loop_server(State) ->
+matrix_server() ->
   receive
-    {Pid, MsgRef, {multiple, Mat1, Mat2}} ->
+    {Pid, MsgRef, {multiple, Mat1, Mat2}} -> % multiple message processing
       spawn(fun() -> matricesMultiply(Mat1, Mat2, Pid, MsgRef) end),
-      loop_server(State);
-    {Pid, MsgRef, get_version} ->
+      matrix_server();
+    {Pid, MsgRef, get_version} -> % get_version message processing
       Pid ! {MsgRef, version_1},
-      loop_server(State);
-    sw_upgrade ->
-      ?MODULE:loop_server(State);
-    shutdown -> ok
+      matrix_server();
+    sw_upgrade -> % sw_upgrade message processing
+      ?MODULE:matrix_server();
+    shutdown -> % do cleanup here
+      cleanup(),
+      exit(shutdown);
+    _ -> % default message processing, prevent mailbox explosion
+      matrix_server()
   end.
 
+cleanup() -> % cleanup here procedure
+  unregister(matrix_server).
 
-shutdown() ->
+shutdown() -> % sending shutdown to server
   matrix_server ! shutdown.
 
 mult(Matrix1, Matrix2) ->
-  ClientPid = self(),
-  matrix_server ! {ClientPid, ClientPid, {multiple, Matrix1, Matrix2}},
-  receive
-    {ClientPid, ResMatrix} -> ResMatrix
+  MsgRef = make_ref(),
+  try matrix_server ! {self(), MsgRef, {multiple, Matrix1, Matrix2}} of
+    _ ->
+      receive
+        {MsgRef, ResMatrix} -> ResMatrix
+      end
+  catch % retry
+    error:_ ->
+      exit(error)
   end.
+
+
+get_version() ->
+  MsgRef = make_ref(),
+  try matrix_server ! {self(), MsgRef, get_version} of
+    _ ->
+      receive
+        {MsgRef, Version} -> Version
+      end
+  catch % retry
+    error:_ ->
+      exit(error)
+  end.
+
+start_server() ->
+  spawn(server_supervisor, restarter, []).
+
+explanation() ->
+  io:format("The supervisor code and the server code should be in separated moduls because
+  we would like to compile and link the upgraded server module as many times as we want without killing the server,
+  becuase Erlang can hold only two versions of code for a single module, but the supervisor process is waiting
+  in a receive it can prevent us to upgrade the server code more than one time if not seperated.
+  When seperating, other alive modules (supervisor) don't distrub upgrading the server module many times.").
---
+++ HW_2/server_supervisor.erl
@@ -0,0 +1,17 @@
+-module(server_supervisor).
+-author("Ido Yehezkel & Ohad Zohar").
+
+%% API
+-export([restarter/0]).
+
+restarter() ->
+  process_flag(trap_exit, true),
+  Pid = spawn_link(matrix_server, matrix_server, []),
+  register(matrix_server, Pid),
+  receive
+    {'EXIT', Pid, normal} -> ok; % no crash
+    {'EXIT', Pid, shutdown} -> ok; % no crash
+    {'EXIT', Pid, _} ->
+      erlang:display("Restarting server"),
+      restarter() % restart
+  end.
